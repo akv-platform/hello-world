cd7b04971aeab962cdd032cbcba29e285e08f541
Mulhearn, Matthew <mmulhe200@cable.comcast.com>
2021-10-24 20:37:25 -0400

    priv

--- NugetConsumer/Class2.cs
+++ NugetConsumer/Class2.cs
@@ -1,12 +1,10 @@
-﻿using hello_world_nuget;
-
-namespace NugetConsumer
+﻿namespace NugetConsumer
 {
     public class Class2
     {
         public Class2()
         {
-            var x = new SomeClass();
+            var x = new hello_world_nuget_priv.PrivClass();
         }
     }
 }
